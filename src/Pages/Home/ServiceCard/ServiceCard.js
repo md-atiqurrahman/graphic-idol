f4d0fa6713fe4a333d0d52843d130c66e950df1f
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2023-03-17 00:55:01 +0600

    add footer

--- src/Pages/Home/ServiceCard/ServiceCard.js
+++ src/Pages/Home/ServiceCard/ServiceCard.js
@@ -5,9 +5,9 @@ const ServiceCard = ({ image, cardTitle }) => {
     return (
         <div className="card justify-center items-center w-[350px] h-[328px] bg-base-100 shadow-cxl 
         p-[25px]">
-            <figure className="mt-[34px]">
+            <div className="mt-[34px]">
                 <img src={image} alt="logo design" />
-            </figure>
+            </div>
             <div className="text-center text-black">
                 <h2 className="text-[21px] font-semibold leading-[78px] mt-[25px] mb-[5px]">{cardTitle}</h2>
                 <div className='flex justify-center items-center cursor-pointer'>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FaBehance, FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+
     return (
-        <div>
-            <h2>This is footer</h2>
+        <div className='flex justify-between items-center bg-primary px-[120px] py-[35px]'>
+            <h6 className='text-white text-[16px] font-normal leading-[24px]'>© {year} Graphic Mart All Rights Reserved</h6>
+            <div className='flex items-center gap-[24px] text-[#B0B0B0] font-normal leading-[1]'>
+                <span className='hover:text-secondary cursor-pointer'>
+                    <FaBehance></FaBehance>
+                </span>
+                <span className='hover:text-secondary cursor-pointer'>
+                    <FaFacebookF></FaFacebookF>
+                </span>
+                <span className='hover:text-secondary cursor-pointer'>
+                    <FaLinkedinIn></FaLinkedinIn>
+                </span>
+                <span className='hover:text-secondary cursor-pointer'>
+                    <FaTwitter></FaTwitter>
+                </span>
+                <span className='hover:text-secondary cursor-pointer'>
+                    <FaInstagram></FaInstagram>
+                </span>
+            </div>
         </div>
     );
 };
